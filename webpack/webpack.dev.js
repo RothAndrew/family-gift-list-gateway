c6297a0c24f211b2b5e99dab186d7f3fc5f88938
Andrew Roth <andrew.roth@saic.com>
2019-03-05 20:24:09 -0500

    Run pre-commit -a

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -49,7 +49,7 @@ module.exports = (options) => webpackMerge(commonConfig({ env: ENV }), {
         '/swagger-resources',
         '/v2/api-docs',
         '/h2-console',
-        '/login',        
+        '/login',
         '/auth'
       ],
       target: `http${options.tls ? 's' : ''}://127.0.0.1:8080`,
